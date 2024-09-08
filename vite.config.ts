4b46b03e06fba0d09e6d92dd262deaa9d283078a
Diyor Safarov <tankisttanjig@gmail.com>
2024-09-09 00:19:54 +0300

    feat: img optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
+import { imageOptimizationConfig } from './src/shared/constants/vite-defualt-configs';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteImageOptimizer(imageOptimizationConfig)],
   css: {
     postcss: {
-      plugins: [autoprefixer({})],
+      plugins: [autoprefixer()],
     },
   },
 });
